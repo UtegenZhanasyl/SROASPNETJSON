a0605a1ec17b850faf396834928d4b73d46ab579
HUAWEI <HUAWEI@LAPTOP-S4L0FLSU>
2025-02-21 16:49:44 +0600

    Добавьте файлы проекта.

---
+++ SROASPNETJSON/Program.cs
@@ -0,0 +1,21 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapGet("/hello", () => new { message = "Hello, World!" });
+
+app.Run();
